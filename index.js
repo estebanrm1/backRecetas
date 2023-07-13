32dff7fbce450aec943cadfcf7379f175067ac3b
estebanrm1 <solsel90@gmail.com>
2023-07-13 02:39:59 -0300

    Se agrego los middlewares necesarios a la configuracion inicial

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
-
+import cors from 'cors'
+import morgan from "morgan";
+import path from 'path'
 //usar un puerto
 const app = express();
 
 app.set('port', process.env.PORT || 4000);
-app.listen(app.get('port'),()=>{
+app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto '+app.get('port'));
 })
 
 //middlewares
+app.use(cors()); //permitir conexiones remotas
+app.use(express.json()); //permite a mi app recibir objetos en formato json en los request
+app.use(morgan('dev')) //muestra en consola info extra de las solicitudes
+app.use(express.static(path.join(__dirname, '/public')))
 
 //rutas
